84871ea8a8925e2d7056b3225825cf95271ee434
Joyce <joycenjoroge47@gmail.com>
2022-11-29 15:18:28 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(pickup){
+        if (pickup<42) {
+            return(42-pickup);
+        }
+        else{
+            return (pickup-42);
+        }
+}
+
+function distanceFromHqInFeet(pickup) {
+    return distanceFromHqInBlocks(pickup)*264;
+}
+
+
+function distanceTravelledInFeet(start,destination){
+    if (start>destination) {
+     return(start-destination)*264;        
+    }
+    else{
+        return (destination-start)*264;
+    }
+    
+}
+
+function calculatesFarePrice(start,destination){
+
+    let distance = distanceTravelledInFeet(start,destination);
+    if (distance<= 400){
+        return 0;
+      }
+      if (distance>=400 && distance<=2000){
+        return (distance-400)* 0.02;
+      }
+      else if(distance>=2000&& distance<=2500){
+        return 25;
+      }
+      else if (distance > 2500){
+        return "cannot travel that far";
+      }
+    }
